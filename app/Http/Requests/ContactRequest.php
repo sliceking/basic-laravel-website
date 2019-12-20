d26b49ab93f6487f92257fc38db4453c32bcdbc1
Stan Wielga <svwielga4@gmail.com>
2019-12-20 15:55:34 -0800

    added a contact form with validations on the specific request

---
+++ app/Http/Requests/ContactRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required|email',
+            'subject' => 'required',
+            'message' => 'required',
+        ];
+    }
+
+    public function messages ()
+    {
+        return [
+            'name.required' => 'You should fill the names out yo.',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'email' => 'email address',
+        ];
+    }
+}
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,4 +2,38 @@
 
 @section('content')
     <h1>contact</h1>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+            </ul>
+        </div>
+    @endif
+    <form method="POST" action="{{ route('contact-form-submit') }}">
+        @csrf
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" name="name" id="name"
+                placeholder="Enter name">
+        </div>
+        <div class="form-group">
+            <label for="email">Email address</label>
+            <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <label for="subject">Subject</label>
+            <input type="text" class="form-control" name="subject" id="subject"
+                placeholder="What do you want to talk about">
+        </div>
+        <div class="form-group">
+            <label for="message">Message</label>
+            <textarea type="text" class="form-control" name="message" id="message"
+                placeholder="Enter your message" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 @endsection
